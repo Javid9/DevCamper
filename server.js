b130421ce48669ae6eea04c6e63188fa1124848e
JavidDadashov <javidfd@code.edu.az>
2021-02-28 04:29:21 +0400

    intro middleware

--- server.js
+++ server.js
@@ -1,31 +1,23 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 
+// Routes file
+const bootcamps = require('./routes/bootcamps')
 
 // Loads env vars
 dotenv.config({path: 'config/config.env'});
 
 const app = express();
 
-app.get('/api/v1/bootcamps', (req, res) =>{
-    res.status(200).json({success: true, msg: 'Show  all bootcamps'});
-});
+// Dev logging middleware
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
 
-app.get('/api/v1/bootcamps/:id', (req, res) =>{
-    res.status(200).json({success: true, msg: `Show  bootcamp ${req.params.id}`});
-});
+// Mount routers
+app.use('/api/v1/bootcamps',bootcamps);
 
-app.post('/api/v1/bootcamps', (req, res) =>{
-    res.status(200).json({success: true, msg: 'Create  new bootcamp'});
-});
-
-app.put('/api/v1/bootcamps/:id', (req, res) =>{
-    res.status(200).json({success: true, msg: `Update  bootcamp ${req.params.id}`});
-});
-
-app.delete('/api/v1/bootcamps/:id', (req, res) =>{
-    res.status(200).json({success: true, msg: `Delete  bootccamp ${req.params.id}`});
-});
 
 const PORT = process.env.PORT || 5000;
 
